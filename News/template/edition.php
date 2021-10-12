c7a671bc6afa96da9ea04ea73013b755f5a38d5a
Vladzislav <58526911+Vhublad@users.noreply.github.com>
2021-10-12 23:11:36 +0300

    Site

---
+++ News/template/edition.php
@@ -0,0 +1,13 @@
+<?php
+	require 'menu.php';
+	require 'header.html';
+?>	<div class="container2">
+		<h1>Редакция</h1>
+		<p>Название: News («news.com»)</p>
+		<p>Учредитель: ООО «news.com»</p>
+		<p>Периодичность: ежедневно, круглосуточно, бесплатно</p>
+		<p>Адрес издателя: г.Минск, ул. Московская д. 251<br>Email: <a href="mailto:someemail@mail.сom">someemail@mail.com</a></p>
+	</div>
+<?php	
+	require 'footer.html';
+?>
